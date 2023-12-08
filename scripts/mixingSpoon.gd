edba3114e13c28c6fc0c1dc45afb4b02e475f563
Sam Feng <fengyuan20000303@gmail.com>
2023-12-08 01:11:02 -0800

    Update mixingSpoon.gd

--- scripts/mixingSpoon.gd
+++ scripts/mixingSpoon.gd
@@ -9,11 +9,6 @@ func _physics_process(delta):
 		rotation = lerp_angle(rotation, deg_to_rad(30), rotateSpeed*delta);
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_body_entered(body):
 	if not $AudioStreamPlayer2D.is_playing():
 		
