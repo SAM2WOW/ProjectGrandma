5c3d0f69a10576f5dcd6e048d6b38c30ea2f89d8
Sam Feng <fengyuan20000303@gmail.com>
2023-12-07 18:52:34 -0800

    Adding New Sounds

--- scripts/mixingSpoon.gd
+++ scripts/mixingSpoon.gd
@@ -12,3 +12,24 @@ func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	if not $AudioStreamPlayer2D.is_playing():
+		
+		# don't play if mass is too small
+		if body == RigidBody2D:
+			if body.get_mass() <= 20:
+				return
+		
+		# get velocity
+		var velocity = get_linear_velocity()
+		var speed = clamp(velocity.length() / 500.0, 0.0, 1.0)
+		var volume = lerp(0.0, 1.0, speed)
+		var pitch = lerp(0.8, 1.2, speed)
+		
+		# set volume and pitch
+		$AudioStreamPlayer2D.volume_db = linear_to_db(volume);
+		$AudioStreamPlayer2D.pitch_scale = pitch;
+		$AudioStreamPlayer2D.play()
+
