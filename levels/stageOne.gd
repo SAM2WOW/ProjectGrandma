57166b5c1134d6e204faf32f4603055770622931
Sam Feng <fengyuan20000303@gmail.com>
2023-12-07 22:54:52 -0800

    Stage One Dialogue Update

--- levels/stageOne.gd
+++ levels/stageOne.gd
@@ -7,10 +7,30 @@ func _ready():
 	
 	Global.textManager.Activate("Hungry")
 	
-	await get_tree().create_timer(2).timeout
+	await get_tree().create_timer(1).timeout
 	
 	Global.textManager.Activate("Adobo")
 	
 	await get_tree().create_timer(2).timeout
 	
 	Global.textManager.Activate("Adobo1")
+
+
+func _on_liquid_draggable_on_mouse_hovering():
+	Global.textManager.Activate("Sauce")
+
+
+func _on_pan_on_mouse_hovering():
+	Global.textManager.Activate("Fire")
+
+
+func _on_container_on_grabbed():
+	Global.textManager.Activate("Item")
+
+
+func _on_container_2_on_grabbed():
+	Global.textManager.Activate("Item2")
+
+
+func _on_liquid_draggable_2_on_grabbed():
+	Global.textManager.Activate("Sauce2")
